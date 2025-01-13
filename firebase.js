3ee00be2170b8ddea4ea6f5e0f87b56ab0c1714c
alik95997 <88495045+alik95997@users.noreply.github.com>
2025-01-13 13:17:33 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
-import { getFirestore, setDoc, doc, getDoc, deleteDoc, getDocs, collection, addDoc, query, where, updateDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js"
+import { getFirestore, setDoc, doc, getDoc, deleteDoc, getDocs, collection, addDoc, query, where, updateDoc,limit } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app)
-export { app, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, getFirestore, deleteDoc, collection, setDoc, doc, db, updateDoc, getDocs, getDoc, addDoc, query, where, signOut }
+export { app, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, limit, getFirestore, deleteDoc, collection, setDoc, doc, db, updateDoc, getDocs, getDoc, addDoc, query, where, signOut }
